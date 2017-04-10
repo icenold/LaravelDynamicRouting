e11bb9954ae144fe39565d11748930f1af766cc6
icenold <arnold.naval99@gmail.com>
2017-04-11 04:02:05 +0800

    Update Routing.php
    Added "<action>_<requestType>"

--- Routing.php
+++ Routing.php
@@ -3,12 +3,20 @@ Convention for controller Names:
 -Every controller should end with the word "Controller"(case sensitive) e.g: 'PersonController','StudentController'
 
 Convention for browser Routes:
--going to "http://domain/Student/Grade" will use the Grade() function from the class StudentController(notice match casing)
--going to "http://domain/students/grades" will use the grades() function from the class studentsController(notice match casing)
+-going to "http://domain/Student/Grade" will use the Grade_get() function from the class StudentController(notice match casing)
+-going to "http://domain/students/grades" will use the grades_get() function from the class studentsController(notice match casing)
 */
 
-Route::get('/{controllerName}/{action}',function(Request $request,$controllerName, $action){
-    $className = "App\Http\Controllers\\$controllerName"."Controller";
-    $controllerObject = new $className();
-    return $controllerObject->$action($request);
+Route::get('/{controller}/{action}',function($controller,$action,Request $request){
+    $className = "App\Http\Controllers\\$controller"."Controller";
+    $classInstance = new $className();
+    $actionName = $action."_get";
+    return $classInstance->$actionName($request);
+});
+
+Route::post('/{controller}/{action}',function($controller,$action,Request $request){
+    $className = "App\Http\Controllers\\$controller"."Controller";
+    $classInstance = new $className();
+    $actionName = $action."_post";
+    return $classInstance->$actionName($request);
 });
